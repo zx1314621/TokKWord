b40a522609100bb46ab0cbb22faf5df911584f9a
zx1314621 <935638374@qq.com>
2020-04-25 11:49:48 -0700

    finished

--- src/main/java/edu/scu/App.java
+++ src/main/java/edu/scu/App.java
@@ -13,36 +13,19 @@ public class App
     private static final int K = 10;
     public static void main( String[] args )
     {
-        Runtime runtime = Runtime.getRuntime();
-        // record application start Memory
-        long preTotalMemory = runtime.totalMemory() / 1024 / 1024;
-
-        // record application start time
-        long a=System.currentTimeMillis();
 
         ReadText readText = new ReadText();
         // Read Save As TreeMap
-        //List<Map.Entry<String, Long>> map =  readText.read(small_txt);
+        //Map<String, Long> map =  readText.read(small_txt);
 
         // Read Save As HashMap
         Map<String, Long> map =  readText.readAsHashMap(small_txt);
 
         //Sort Firstly, then get TopK
-        //readText.getTopK(map, K);
+        readText.getTopK(map, K);
 
         //get Directly, K loop, K max
-        readText.getTopKDirectly(map, K);
-
-
-        // record application end Memory
-        long totalMemory = runtime.totalMemory() / 1024 / 1024;
-
-        // print Memory usage
-        System.out.println("Memory usage: " + (totalMemory - preTotalMemory) + " M");
-
-
-        // print running time
-        System.out.println("running time: "+(System.currentTimeMillis()-a)/1000f+" second ");
+        //readText.getTopKDirectly(map, K);
 
     }
 
