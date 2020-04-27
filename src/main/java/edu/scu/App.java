efe06359784aa650c3279815510ccbf512fecaae
zx1314621 <935638374@qq.com>
2020-04-26 20:52:30 -0700

    finsh version

--- src/main/java/edu/scu/App.java
+++ src/main/java/edu/scu/App.java
@@ -7,20 +7,28 @@ import java.util.Map;
 
 public class App 
 {
+    // data and code file save in same directory
     private static final String small_txt = "../data/dataset-400MB.txt";
     private static final String medium_txt = "../data/dataset-8GB.txt";
     private static final String large_txt = "../data/Big Data.txt";
+
+    // K parameter
     private static final int K = 10;
     public static void main( String[] args )
     {
 
         ReadText readText = new ReadText();
+
+
+        // The Two lines represents two different store data structure
         // Read Save As TreeMap
         Map<String, Long> map =  readText.read(small_txt);
 
         // Read Save As HashMap
         //Map<String, Long> map =  readText.readAsHashMap(medium_txt);
 
+
+        // The Two lines represents two get algorithm
         //Sort Firstly, then get TopK
         readText.getTopK(map, K);
 
